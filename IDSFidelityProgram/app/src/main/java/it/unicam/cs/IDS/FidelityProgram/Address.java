079e406fd05d1caf9b84ca3db934bd59703b4b62
Anthony Eleuteri <anthony.eleuteri@studenti.unicam.it>
2023-04-17 17:36:08 +0200

    primo

--- IDSFidelityProgram/app/src/main/java/it/unicam/cs/IDS/FidelityProgram/Address.java
+++ IDSFidelityProgram/app/src/main/java/it/unicam/cs/IDS/FidelityProgram/Address.java
@@ -1,21 +1,24 @@
 package it.unicam.cs.IDS.FidelityProgram;
 
-
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import javax.xml.namespace.QName;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
-
+import 
 /**
- *Simple class to store an address
+ * Simple class to store an address
  *
  * @Authors Kacper Henryk Osicki, Anthony Eleuteri, Yuri Orsili
  */
@@ -24,78 +27,64 @@ public class Address {
     private int number;
     private int zipCode;
     private String city;
-    private String country;
     private String province;
+    private String country;
 
-    private final String path = "/Users/kacperosicki/Desktop/IDSFidelityProgram/LoyalityPlatform/IDSFidelityProgram/app/src/main/resources/Comuni.json";
+    private static final String PATH = "/Users/kacperosicki/Desktop/IDSFidelityProgram/LoyalityPlatform/IDSFidelityProgram/app/src/main/resources/Comuni.json";
+    private static final JSONParser parser = new JSONParser();
+    private static final Gson gson = new Gson();
+    private static final JsonObject jsonObject;
 
-    Gson gson = new Gson();
+    static {
+        // Load JSON file as a resource
+        try {
+            jsonObject = gson.fromJson(new FileReader(PATH), JsonObject.class);
+        } catch (IOException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
 
-    // Leggi il file JSON come risorsa del tuo progetto
-    InputStream inputStream = getResourceAsStream("resources/Comuni.json");
-    InputStreamReader reader = new InputStreamReader(inputStream);
-
-    // Converte il file JSON in un oggetto JsonObject
-    JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
-
-    List<String> capList = new ArrayList<>();    JSONParser parser = new JSONParser();
-    private final Object comuni = parser.parse(new FileReader(path));
-    JSONArray zip =  (JSONArray)comuni;
-
-
-    public Address(String street, int number, String zipCode, String city, String country, String province) throws IOException, ParseException {
+    public Address(String street, int number, int zipCode, String city, String country, String province) throws IOException, ParseException {
         Objects.requireNonNull(street);
         Objects.requireNonNull(city);
         Objects.requireNonNull(country);
         Objects.requireNonNull(province);
-        if(street.length()<50){
-            this.street=street;
-        }if(number<1000) {
+
+        if (street.length() < 50) {
+            this.street = street;
+        }
+        if (number < 1000) {
             this.number = number;
         }
-        for(Object s : zip){
-        }pa
-            if(zip.contains(zipCode)){
-                System.out.println("ci sono!");
 
+        // Leggi il file JSON dei comuni e crea una mappa con il nome del comune come chiave e l'oggetto Comune come valore
+        Map<String, Comune> comuniMap = new HashMap<>();
+        Gson gson = new Gson();
+        InputStream inputStream = getClass().getResourceAsStream("/Comuni.json");
+        InputStreamReader reader = new InputStreamReader(inputStream);
+        JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
+        JsonArray comuniArray = jsonObject.getAsJsonArray("comuni");
 
-        }if(!city.isBlank() ||  city.length()>3 ){
-            this.city=city;
-        }if(!country.isBlank() || country.length()>3 ) {
-            this.country = country;
-        }if (province.length()<3){
-            this.province = province;
+        for (JsonElement comuneElement : comuniArray) {
+            JsonObject comuneObject = comuneElement.getAsJsonObject();
+            String nomeComune = comuneObject.get("nome").getAsString();
+            String provincia = comuneObject.get("provincia").getAsString();
+            String cap = comuneObject.get("cap").getAsString();
+            Comune comune = new Comune(provincia, cap);
+            comuniMap.put(nomeComune, comune);
         }
 
-
-
-
-
+        // Verifica che il CAP sia corretto per la provincia e il comune specificati
+        Comune comune = comuniMap.get(city);
+        if (comune != null && comune.getProvince().equals(province) && comune.getZipcode().equals(String.valueOf(zipCode))) {
+            this.zipCode = zipCode;
+            this.city = city;
+            this.province = province;
+        }else{
+            System.out.println();
+        }
     }
 
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getProvince() {
-        return province;
-    }
-
-    public void setProvince(String province) {
-        this.province = province;
-    }
 
     public String getStreet() {
         return street;
@@ -121,9 +110,27 @@ public class Address {
         this.zipCode = zipCode;
     }
 
+    public String getCity() {
+        return city;
+    }
 
+    public void setCity(String city) {
+        this.city = city;
+    }
 
+    public String getProvince() {
+        return province;
+    }
 
+    public void setProvince(String province) {
+        this.province = province;
+    }
 
+    public String getCountry() {
+        return country;
+    }
 
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
