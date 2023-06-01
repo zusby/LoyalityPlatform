ed2e61029dfce85c677b014c339fea7a469183d3
Anthony Eleuteri <anthony.eleuteri@studenti.unicam.it>
2023-06-01 17:52:50 +0200

    testing

--- FidelityPlatform/src/main/java/it/unicam/cs/ids/Shop/Shop.java
+++ FidelityPlatform/src/main/java/it/unicam/cs/ids/Shop/Shop.java
@@ -16,6 +16,7 @@ public class Shop {
     private FidelitySpace space;
     private Catalogue catalogue;
 
+
     public Shop(String id, Address shopAddress, String vatNumber, String name, String shopTelephoneNumber, List<String> shopOwnerIDs) {
         this.id = id;
         this.vatNumber = vatNumber;
@@ -29,54 +30,39 @@ public class Shop {
     public Shop() {
     }
 
-
     public PrizeAwards getPrizes() {
         return prizes;
     }
-
     public void setPrizes(PrizeAwards prizes) {
         this.prizes = prizes;
     }
-
     public FidelitySpace getSpace() {
         return space;
     }
-
     public void setSpace(FidelitySpace space) {
         this.space = space;
     }
-
     public String getId() {
         return id;
     }
-
     public void setId(String id) {
         this.id = id;
     }
-
-
     public List<String> getShopOwners() {
         return this.shopOwners;
     }
-
     public void setShopOwners(List<String> owners) {
         this.shopOwners = owners;
     }
-
-
-
     public String getVatNumber() {
         return vatNumber;
     }
-
     public void setVatNumber(String vatNumber) {
         this.vatNumber = vatNumber;
     }
-
     public List<FidelityProgram> getFidelityPrograms() {
         return fidelityPrograms;
     }
-
     public void setFidelityPrograms(List<FidelityProgram> fidelityPrograms) {
         this.fidelityPrograms = fidelityPrograms;
 
@@ -103,5 +89,4 @@ public class Shop {
                 ", vatNumber='" + vatNumber + '\'' +
                 '}';
     }
-
 }
